06750e92b52e2d4f6e285778b6a704954c24b1cd
m-hamzashakeel <hamza.6.shakeel.com>
2020-07-06 01:28:13 +0500

    screen orientation locked

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:covid19/views/dashboard.dart';
+import 'package:flutter/services.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 void main() => runApp(MyApp());
@@ -27,6 +28,9 @@ final lightTheme = ThemeData(
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
